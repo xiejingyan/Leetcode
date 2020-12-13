ed1bb06b9b96be2da4f87b6e3ee1909782862567
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2020-12-13 10:16:27 +0800

    第217题 排序和哈希表查找重复元素

---
+++ training/src/leetcode/_217_containsDuplicate.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class _217_containsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*
+    排序
+     */
+    public boolean containsDuplicate1(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*
+    哈希表
+     */
+    public boolean containsDuplicate2(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int x : nums) {
+            if (!set.add(x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
