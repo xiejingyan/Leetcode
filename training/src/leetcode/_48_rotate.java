8cc1cbacd7fd3589c57288502bb6aac1c5ccd45d
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2020-12-20 11:00:28 +0800

    第48题 辅助数组、原地旋转、翻转

---
+++ training/src/leetcode/_48_rotate.java
@@ -0,0 +1,72 @@
+package leetcode;
+
+public class _48_rotate {
+    public void rotate(int[][] matrix) {
+        int[] list = new int[matrix.length * matrix.length];
+        int tmp = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = matrix.length - 1; j >= 0; j--) {
+                list[tmp] = matrix[j][i];
+                tmp++;
+            }
+        }
+        tmp = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = list[tmp];
+                tmp++;
+            }
+        }
+    }
+
+//    辅助数组
+    public void rotate1(int[][] matrix) {
+        int n = matrix.length;
+        int[][] matrix_new = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix_new[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix[i][j] = matrix_new[i][j];
+            }
+        }
+    }
+
+//    原地旋转
+    public void rotate2(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < (n + 1) / 2; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+
+//    翻转
+    public void rotate3(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
