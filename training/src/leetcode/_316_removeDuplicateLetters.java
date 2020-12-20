f1ca0d04e12b726b7612428d1a16b1989a865dc6
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2020-12-20 10:40:02 +0800

    第316题 贪心+栈

--- training/src/leetcode/_316_removeDuplicateLetters.java
+++ training/src/leetcode/_316_removeDuplicateLetters.java
@@ -1,2 +1,30 @@
-package leetcode;public class _316_removeDuplicateLetters {
+package leetcode;
+
+public class _316_removeDuplicateLetters {
+    public String removeDuplicateLetters1(String s) {
+        boolean[] vis = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!vis[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        vis[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                vis[ch - 'a'] = true;
+                sb.append(ch);
+            }
+            num[ch - 'a'] -= 1;
+        }
+        return sb.toString();
+    }
 }
