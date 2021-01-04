4a460783b577091dced193a33dd4cf699d8986fb
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-01-04 16:11:59 +0800

    第509题 动态规划、矩阵快速幂、通项公式

---
+++ training/src/leetcode/_509_fib.java
@@ -0,0 +1,68 @@
+package leetcode;
+
+public class _509_fib {
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int a = 0, b = 1;
+        for (int i = 1; i < n; i++) {
+            a += b;
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        return b;
+    }
+
+//    动态规划
+    public int fib1(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int p = 0, q = 0, r = 1;
+        for (int i = 2; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+
+//    矩阵快速幂
+    public int fib2(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int[][] q = {{1, 1}, {1, 0}};
+        int[][] res = pow(q, n - 1);
+        return res[0][0];
+    }
+    public int[][] pow(int[][] a, int n) {
+        int[][] ret = {{1, 0}, {0, 1}};
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                ret = multiply(ret, a);
+            }
+            n >>= 1;
+            a = multiply(a, a);
+        }
+        return ret;
+    }
+    public int[][] multiply(int[][] a, int[][] b) {
+        int[][] c = new int[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j];
+            }
+        }
+        return c;
+    }
+
+//    通项公式
+    public int fib3(int n) {
+        double sqrt5 = Math.sqrt(5);
+        double fibN = Math.pow((1 + sqrt5) / 2, n) - Math.pow((1 - sqrt5) / 2, n);
+        return (int) Math.round(fibN / sqrt5);
+    }
+}
