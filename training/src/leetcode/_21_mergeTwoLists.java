0db19a4949d86bb5ba5e4af19b726e73072b1a98
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-02-19 11:35:51 +0800

    第21题 递归、迭代

---
+++ training/src/leetcode/_21_mergeTwoLists.java
@@ -0,0 +1,63 @@
+package leetcode;
+
+public class _21_mergeTwoLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode tmp = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                tmp.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                tmp.next = l2;
+                l2 = l2.next;
+            }
+            tmp = tmp.next;
+        }
+        if (l1 != null) {
+            tmp.next = l1;
+        }
+        if (l2 != null) {
+            tmp.next = l2;
+        }
+        return head.next;
+    }
+
+//    递归
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists1(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists1(l1, l2.next);
+            return l2;
+        }
+    }
+
+//    迭代
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
+}
