922ebbed109d01f898e9b2544059df6a38d0d944
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-02-24 11:13:18 +0800

    第28题 子串比较、双指针、Rabin Karp

---
+++ training/src/leetcode/_28_strStr.java
@@ -0,0 +1,97 @@
+package leetcode;
+
+public class _28_strStr {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (needle.equals(haystack.substring(i, i + needle.length()))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+//    子串比较
+    public int strStr1(String haystack, String needle) {
+        int L = needle.length(), n = haystack.length();
+
+        for (int start = 0; start < n - L + 1; ++start) {
+            if (haystack.substring(start, start + L).equals(needle)) {
+                return start;
+            }
+        }
+        return -1;
+    }
+
+//    双指针
+    public int strStr2(String haystack, String needle) {
+        int L = needle.length(), n = haystack.length();
+        if (L == 0) return 0;
+
+        int pn = 0;
+        while (pn < n - L + 1) {
+            // find the position of the first needle character
+            // in the haystack string
+            while (pn < n - L + 1 && haystack.charAt(pn) != needle.charAt(0)) ++pn;
+
+            // compute the max match string
+            int currLen = 0, pL = 0;
+            while (pL < L && pn < n && haystack.charAt(pn) == needle.charAt(pL)) {
+                ++pn;
+                ++pL;
+                ++currLen;
+            }
+
+            // if the whole needle string is found,
+            // return its start position
+            if (currLen == L) return pn - L;
+
+            // otherwise, backtrack
+            pn = pn - currLen + 1;
+        }
+        return -1;
+    }
+
+    public int charToInt(int idx, String s) {
+        return (int)s.charAt(idx) - (int)'a';
+    }
+
+//    Rabin Karp
+    public int strStr3(String haystack, String needle) {
+        int L = needle.length(), n = haystack.length();
+        if (L > n) return -1;
+
+        // base value for the rolling hash function
+        int a = 26;
+        // modulus value for the rolling hash function to avoid overflow
+        long modulus = (long)Math.pow(2, 31);
+
+        // compute the hash of strings haystack[:L], needle[:L]
+        long h = 0, ref_h = 0;
+        for (int i = 0; i < L; ++i) {
+            h = (h * a + charToInt(i, haystack)) % modulus;
+            ref_h = (ref_h * a + charToInt(i, needle)) % modulus;
+        }
+        if (h == ref_h) return 0;
+
+        // const value to be used often : a**L % modulus
+        long aL = 1;
+        for (int i = 1; i <= L; ++i) aL = (aL * a) % modulus;
+
+        for (int start = 1; start < n - L + 1; ++start) {
+            // compute rolling hash in O(1) time
+            h = (h * a - charToInt(start - 1, haystack) * aL
+                    + charToInt(start + L - 1, haystack)) % modulus;
+            if (h == ref_h) return start;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String h = "hello", n = "ll";
+        _28_strStr ss = new _28_strStr();
+        System.out.println(ss.strStr(h, n));
+    }
+}
