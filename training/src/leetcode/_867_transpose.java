023cf1fa14797b415a059165c1f37fa94aab7866
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-02-25 09:35:09 +0800

    第867题 遍历

---
+++ training/src/leetcode/_867_transpose.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+public class _867_transpose {
+    public int[][] transpose(int[][] matrix) {
+        if (matrix.length == matrix[0].length) {
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = i + 1; j < matrix[i].length; j++) {
+                    int tmp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = tmp;
+                }
+            }
+            return matrix;
+        }
+        int[][] res = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+
+//    遍历
+    public int[][] transpose1(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] transposed = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+}
