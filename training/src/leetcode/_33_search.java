d05c9beae0a0a4de59796bb8ecc70c5ebdcee15f
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-02-26 10:54:42 +0800

    第33题 二分查找

---
+++ training/src/leetcode/_33_search.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+public class _33_search {
+    public int search(int[] nums, int target) {
+        int res = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                res = i;
+                break;
+            }
+        }
+        return res;
+    }
+
+//    二分查找
+    public int search1(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
