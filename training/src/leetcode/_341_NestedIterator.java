90f85b0871553cb5a2658f824951bbb009460807
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-03-23 10:41:31 +0800

    第94题 深度优先搜索、栈

---
+++ training/src/leetcode/_341_NestedIterator.java
@@ -0,0 +1,75 @@
+package leetcode;
+
+import java.util.*;
+
+public class _341_NestedIterator implements Iterator<Integer> {
+//    深度优先搜索
+    private List<Integer> vals;
+    private Iterator<Integer> cur;
+//    public NestedIterator(List<NestedInteger> nestedList) {
+//        vals = new ArrayList<Integer>();
+//        dfs(nestedList);
+//        cur = vals.iterator();
+//    }
+    @Override
+    public Integer next() {
+        return cur.next();
+    }
+    @Override
+    public boolean hasNext() {
+        return cur.hasNext();
+    }
+    private void dfs(List<NestedInteger> nestedList) {
+        for (NestedInteger nest : nestedList) {
+            if (nest.isInteger()) {
+                vals.add(nest.getInteger());
+            } else {
+                dfs(nest.getList());
+            }
+        }
+    }
+
+//    栈
+    // 存储列表的当前遍历位置
+//    private Deque<Iterator<NestedInteger>> stack;
+//    public NestedIterator(List<NestedInteger> nestedList) {
+//        stack = new LinkedList<Iterator<NestedInteger>>();
+//        stack.push(nestedList.iterator());
+//    }
+//    @Override
+//    public Integer next() {
+//        // 由于保证调用 next 之前会调用 hasNext，直接返回栈顶列表的当前元素
+//        return stack.peek().next().getInteger();
+//    }
+//    @Override
+//    public boolean hasNext() {
+//        while (!stack.isEmpty()) {
+//            Iterator<NestedInteger> it = stack.peek();
+//            if (!it.hasNext()) { // 遍历到当前列表末尾，出栈
+//                stack.pop();
+//                continue;
+//            }
+//            // 若取出的元素是整数，则通过创建一个额外的列表将其重新放入栈中
+//            NestedInteger nest = it.next();
+//            if (nest.isInteger()) {
+//                List<NestedInteger> list = new ArrayList<NestedInteger>();
+//                list.add(nest);
+//                stack.push(list.iterator());
+//                return true;
+//            }
+//            stack.push(nest.getList().iterator());
+//        }
+//        return false;
+//    }
+}
+
+interface NestedInteger {
+    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    public boolean isInteger();
+    // @return the single integer that this NestedInteger holds, if it holds a single integer
+    // Return null if this NestedInteger holds a nested list
+    public Integer getInteger();
+    // @return the nested list that this NestedInteger holds, if it holds a nested list
+    // Return null if this NestedInteger holds a single integer
+    public List<NestedInteger> getList();
+}
