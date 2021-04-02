69f5f439b866dd18c1a6008cbb5980281c879fe2
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-04-02 09:52:15 +0800

    第300题 动态规划、贪心+二分查找

---
+++ training/src/leetcode/_300_lengthOfLIS.java
@@ -0,0 +1,70 @@
+package leetcode;
+
+public class _300_lengthOfLIS {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            int tmp = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    tmp = Math.max(tmp, dp[j]);
+                }
+            }
+            dp[i] = tmp + 1;
+        }
+        int res = 0;
+        for (int j : dp) {
+            res = Math.max(res, j);
+        }
+        return res;
+    }
+
+//    动态规划
+    public int lengthOfLIS1(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+
+//    贪心+二分查找
+    public int lengthOfLIS2(int[] nums) {
+        int len = 1, n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] d = new int[n + 1];
+        d[len] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len, pos = 0; // 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
+}
