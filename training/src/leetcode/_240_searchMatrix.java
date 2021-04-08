109e6768ef20d5722e9351357a6c7bf05de53996
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-04-08 10:58:17 +0800

    第240题 暴力、二分查找、缩减搜索空间、排序二叉树

---
+++ training/src/leetcode/_240_searchMatrix.java
@@ -0,0 +1,134 @@
+package leetcode;
+
+public class _240_searchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int i = m - 1, j = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            else if (matrix[i][j] < target) {
+                j++;
+            }
+            else {
+                i--;
+            }
+        }
+        return false;
+    }
+
+//    暴力
+    public boolean searchMatrix1(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+//    二分查找
+    private boolean binarySearch(int[][] matrix, int target, int start, boolean vertical) {
+        int lo = start;
+        int hi = vertical ? matrix[0].length-1 : matrix.length-1;
+
+        while (hi >= lo) {
+            int mid = (lo + hi)/2;
+            if (vertical) { // searching a column
+                if (matrix[start][mid] < target) {
+                    lo = mid + 1;
+                } else if (matrix[start][mid] > target) {
+                    hi = mid - 1;
+                } else {
+                    return true;
+                }
+            } else { // searching a row
+                if (matrix[mid][start] < target) {
+                    lo = mid + 1;
+                } else if (matrix[mid][start] > target) {
+                    hi = mid - 1;
+                } else {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        // an empty matrix obviously does not contain `target`
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        // iterate over matrix diagonals
+        int shorterDim = Math.min(matrix.length, matrix[0].length);
+        for (int i = 0; i < shorterDim; i++) {
+            boolean verticalFound = binarySearch(matrix, target, i, true);
+            boolean horizontalFound = binarySearch(matrix, target, i, false);
+            if (verticalFound || horizontalFound) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+//    缩减搜索空间
+    private int[][] matrix;
+    private int target;
+    private boolean searchRec(int left, int up, int right, int down) {
+        // this submatrix has no height or no width.
+        if (left > right || up > down) {
+            return false;
+            // `target` is already larger than the largest element or smaller
+            // than the smallest element in this submatrix.
+        } else if (target < matrix[up][left] || target > matrix[down][right]) {
+            return false;
+        }
+
+        int mid = left + (right-left)/2;
+
+        // Locate `row` such that matrix[row-1][mid] < target < matrix[row][mid]
+        int row = up;
+        while (row <= down && matrix[row][mid] <= target) {
+            if (matrix[row][mid] == target) {
+                return true;
+            }
+            row++;
+        }
+        return searchRec(left, row, mid-1, down) || searchRec(mid+1, up, right, row-1);
+    }
+    public boolean searchMatrix3(int[][] mat, int targ) {
+        // cache input values in object to avoid passing them unnecessarily
+        // to `searchRec`
+        matrix = mat;
+        target = targ;
+
+        // an empty matrix obviously does not contain `target`
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+
+        return searchRec(0, 0, matrix[0].length-1, matrix.length-1);
+    }
+
+//    排序二叉树
+    public boolean searchMatrix4(int[][] matrix, int target) {
+        // start our "pointer" in the bottom-left
+        int row = matrix.length-1;
+        int col = 0;
+
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] > target) {
+                row--;
+            } else if (matrix[row][col] < target) {
+                col++;
+            } else { // found it
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
