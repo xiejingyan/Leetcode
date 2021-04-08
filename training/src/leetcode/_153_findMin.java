c88a73c690448d34185ae9638627a346c57b1ef5
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-04-08 10:01:56 +0800

    第153题 二分查找

---
+++ training/src/leetcode/_153_findMin.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class _153_findMin {
+    public int findMin(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                return nums[i];
+            }
+        }
+        return res;
+    }
+
+//    二分查找
+    public int findMin1(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (nums[pivot] < nums[high]) {
+                high = pivot;
+            } else {
+                low = pivot + 1;
+            }
+        }
+        return nums[low];
+    }
+}
