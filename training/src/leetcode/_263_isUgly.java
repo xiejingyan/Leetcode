5ed95ca496f7752e5747f6c111bc5d33b00dbe99
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-04-10 09:29:52 +0800

    第263题 数学

---
+++ training/src/leetcode/_263_isUgly.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+public class _263_isUgly {
+    public boolean isUgly(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        while (n % 5 == 0) {
+            n /= 5;
+        }
+        return n == 1;
+    }
+
+//    数学
+    public boolean isUgly1(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        int[] factors = {2, 3, 5};
+        for (int factor : factors) {
+            while (n % factor == 0) {
+                n /= factor;
+            }
+        }
+        return n == 1;
+    }
+}
