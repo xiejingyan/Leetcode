2040912dfec5b98bba9ce7f898f26796baa20482
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-04-12 10:20:33 +0800

    第162题 线性扫描、递归二分、迭代二分

---
+++ training/src/leetcode/_162_findPeakElement.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+public class _162_findPeakElement {
+    public int findPeakElement(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return i;
+            }
+        }
+        return nums.length - 1;
+    }
+
+//    线性扫描
+    public int findPeakElement1(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1])
+                return i;
+        }
+        return nums.length - 1;
+    }
+
+//    递归二分
+    public int findPeakElement2(int[] nums) {
+        return search(nums, 0, nums.length - 1);
+    }
+    public int search(int[] nums, int l, int r) {
+        if (l == r)
+            return l;
+        int mid = (l + r) / 2;
+        if (nums[mid] > nums[mid + 1])
+            return search(nums, l, mid);
+        return search(nums, mid + 1, r);
+    }
+
+//    迭代二分
+    public int findPeakElement3(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+}
