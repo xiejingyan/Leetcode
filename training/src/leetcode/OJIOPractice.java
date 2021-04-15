66b33de166869c0dd653ae7aae862d8d94d80c1c
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-04-15 20:45:09 +0800

    OJ IO练习

---
+++ training/src/leetcode/OJIOPractice.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class OJIOPractice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            Long a = sc.nextLong();
+            Long b = sc.nextLong();
+            System.out.println(a + b);
+        }
+    }
+}
