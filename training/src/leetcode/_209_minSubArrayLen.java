7c5de3e547b3a6f0f781fc35add3d121500f4165
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-04-16 11:27:55 +0800

    第209题 暴力、前缀和+二分查找、滑动窗口

---
+++ training/src/leetcode/_209_minSubArrayLen.java
@@ -0,0 +1,92 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class _209_minSubArrayLen {
+    public int minSubArrayLen(int target, int[] nums) {
+        int l = 0, r = 0, tmp = 0, res = Integer.MAX_VALUE;
+        for (; r < nums.length; r++) {
+            tmp += nums[r];
+            if (tmp >= target) {
+                while (tmp >= target) {
+                    tmp -= nums[l];
+                    l++;
+                }
+                res = Math.min(res, r - l + 2);
+                if (res == 1) {
+                    break;
+                }
+            }
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+//    暴力
+    public int minSubArrayLen1(int s, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if (sum >= s) {
+                    ans = Math.min(ans, j - i + 1);
+                    break;
+                }
+            }
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+
+//    前缀和+二分查找
+    public int minSubArrayLen2(int s, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int[] sums = new int[n + 1];
+        // 为了方便计算，令 size = n + 1
+        // sums[0] = 0 意味着前 0 个元素的前缀和为 0
+        // sums[1] = A[0] 前 1 个元素的前缀和为 A[0]
+        // 以此类推
+        for (int i = 1; i <= n; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+        for (int i = 1; i <= n; i++) {
+            int target = s + sums[i - 1];
+            int bound = Arrays.binarySearch(sums, target);
+            if (bound < 0) {
+                bound = -bound - 1;
+            }
+            if (bound <= n) {
+                ans = Math.min(ans, bound - (i - 1));
+            }
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+
+//    滑动窗口
+    public int minSubArrayLen3(int s, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += nums[end];
+            while (sum >= s) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+}
