4aa2fa6a93b41c1b3abd4e407388c01244f05672
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-04-16 10:59:32 +0800

    第145题 递归、迭代、Morris遍历

--- training/src/leetcode/_145_postorderTraversal.java
+++ training/src/leetcode/_145_postorderTraversal.java
@@ -72,7 +72,7 @@ public class _145_postorderTraversal {
     }
 
 //    迭代
-    public List<Integer> postorderTraversal(TreeNode root) {
+    public List<Integer> postorderTraversal2(TreeNode root) {
         List<Integer> res = new ArrayList<Integer>();
         if (root == null) {
             return res;
@@ -95,5 +95,51 @@ public class _145_postorderTraversal {
             }
         }
         return res;
+    }
+
+//    Morris 遍历
+    public List<Integer> postorderTraversal3(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+
+        TreeNode p1 = root, p2 = null;
+
+        while (p1 != null) {
+            p2 = p1.left;
+            if (p2 != null) {
+                while (p2.right != null && p2.right != p1) {
+                    p2 = p2.right;
+                }
+                if (p2.right == null) {
+                    p2.right = p1;
+                    p1 = p1.left;
+                    continue;
+                } else {
+                    p2.right = null;
+                    addPath(res, p1.left);
+                }
+            }
+            p1 = p1.right;
+        }
+        addPath(res, root);
+        return res;
+    }
+    public void addPath(List<Integer> res, TreeNode node) {
+        int count = 0;
+        while (node != null) {
+            ++count;
+            res.add(node.val);
+            node = node.right;
+        }
+        int left = res.size() - count, right = res.size() - 1;
+        while (left < right) {
+            int temp = res.get(left);
+            res.set(left, res.get(right));
+            res.set(right, temp);
+            left++;
+            right--;
+        }
     }
 }
