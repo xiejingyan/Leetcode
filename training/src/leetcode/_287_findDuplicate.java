f4ef99271bea8d63e447ebdebff41bab3db2b476
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-04-17 11:07:43 +0800

    第287题 二分查找、二进制、快慢指针

---
+++ training/src/leetcode/_287_findDuplicate.java
@@ -0,0 +1,78 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class _287_findDuplicate {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int res = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                res = nums[i];
+                break;
+            }
+        }
+        return res;
+    }
+
+//    二分查找
+    public int findDuplicate1(int[] nums) {
+        int n = nums.length;
+        int l = 1, r = n - 1, ans = -1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            int cnt = 0;
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] <= mid) {
+                    cnt++;
+                }
+            }
+            if (cnt <= mid) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+
+//    二进制
+    public int findDuplicate2(int[] nums) {
+        int n = nums.length, ans = 0;
+        int bit_max = 31;
+        while (((n - 1) >> bit_max) == 0) {
+            bit_max -= 1;
+        }
+        for (int bit = 0; bit <= bit_max; ++bit) {
+            int x = 0, y = 0;
+            for (int i = 0; i < n; ++i) {
+                if ((nums[i] & (1 << bit)) != 0) {
+                    x += 1;
+                }
+                if (i >= 1 && ((i & (1 << bit)) != 0)) {
+                    y += 1;
+                }
+            }
+            if (x > y) {
+                ans |= 1 << bit;
+            }
+        }
+        return ans;
+    }
+
+//    快慢指针
+    public int findDuplicate3(int[] nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
