609f158d853f0b39f4e1d584c38865379362cba9
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-04-18 10:26:31 +0800

    第74题 二分查找

---
+++ training/src/leetcode/_74_searchMatrix.java
@@ -0,0 +1,78 @@
+package leetcode;
+
+public class _74_searchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (target < matrix[0][0] || target > matrix[matrix.length - 1][matrix[0].length - 1]) {
+            return false;
+        }
+        int i = 1;
+        for (; i < matrix.length; i++) {
+            if (matrix[i][0] > target) {
+                break;
+            }
+        }
+        i--;
+        for (int j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            else if (matrix[i][j] > target) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+//    两次二分查找
+    public boolean searchMatrix1(int[][] matrix, int target) {
+        int rowIndex = binarySearchFirstColumn(matrix, target);
+        if (rowIndex < 0) {
+            return false;
+        }
+        return binarySearchRow(matrix[rowIndex], target);
+    }
+    public int binarySearchFirstColumn(int[][] matrix, int target) {
+        int low = -1, high = matrix.length - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (matrix[mid][0] <= target) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+    public boolean binarySearchRow(int[] row, int target) {
+        int low = 0, high = row.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            if (row[mid] == target) {
+                return true;
+            } else if (row[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+
+//    一次二分查找
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int x = matrix[mid / n][mid % n];
+            if (x < target) {
+                low = mid + 1;
+            } else if (x > target) {
+                high = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
