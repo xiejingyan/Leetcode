8b01c8dbfb59b4eecc201af263faa8bf7cd5e2dd
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-04-18 10:14:13 +0800

    第152题 动态规划

--- training/src/leetcode/_152_maxProduct.java
+++ training/src/leetcode/_152_maxProduct.java
@@ -2,13 +2,15 @@ package leetcode;
 
 public class _152_maxProduct {
     public int maxProduct(int[] nums) {
-        int res = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            int tmp = 1;
-            for (int j = i; j < nums.length; j++) {
-                tmp *= nums[j];
-                res = Math.max(res, tmp);
-            }
+        int[] max = new  int[nums.length];
+        int[] min = new  int[nums.length];
+        max[0] = nums[0];
+        min[0] = nums[0];
+        int res = max[0];
+        for (int i = 1; i < nums.length; i++) {
+            max[i] = Math.max(max[i - 1] * nums[i], Math.max(nums[i], min[i - 1] * nums[i]));
+            min[i] = Math.min(min[i - 1] * nums[i], Math.min(nums[i], max[i - 1] * nums[i]));
+            res = Math.max(res, max[i]);
         }
         return res;
     }
