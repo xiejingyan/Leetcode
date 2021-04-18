4c8bb18e84c5215b4d23d315bb1bad29c7e82615
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-04-18 10:07:53 +0800

    第152题 动态规划

---
+++ training/src/leetcode/_152_maxProduct.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+public class _152_maxProduct {
+    public int maxProduct(int[] nums) {
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int tmp = 1;
+            for (int j = i; j < nums.length; j++) {
+                tmp *= nums[j];
+                res = Math.max(res, tmp);
+            }
+        }
+        return res;
+    }
+
+//    动态规划
+    public int maxProduct1(int[] nums) {
+        int length = nums.length;
+        int[] maxF = new int[length];
+        int[] minF = new int[length];
+        System.arraycopy(nums, 0, maxF, 0, length);
+        System.arraycopy(nums, 0, minF, 0, length);
+        for (int i = 1; i < length; ++i) {
+            maxF[i] = Math.max(maxF[i - 1] * nums[i], Math.max(nums[i], minF[i - 1] * nums[i]));
+            minF[i] = Math.min(minF[i - 1] * nums[i], Math.min(nums[i], maxF[i - 1] * nums[i]));
+        }
+        int ans = maxF[0];
+        for (int i = 1; i < length; ++i) {
+            ans = Math.max(ans, maxF[i]);
+        }
+        return ans;
+    }
+    public int maxProduct2(int[] nums) {
+        int maxF = nums[0], minF = nums[0], ans = nums[0];
+        int length = nums.length;
+        for (int i = 1; i < length; ++i) {
+            int mx = maxF, mn = minF;
+            maxF = Math.max(mx * nums[i], Math.max(nums[i], mn * nums[i]));
+            minF = Math.min(mn * nums[i], Math.min(nums[i], mx * nums[i]));
+            ans = Math.max(maxF, ans);
+        }
+        return ans;
+    }
+}
