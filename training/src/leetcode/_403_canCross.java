29bee4a8156f1e1f2c6eae913f3c7a5be22b1854
Pengpeng Xin <xin_pengpeng@bupt.edu.cn>
2021-04-29 10:55:14 +0800

    第403题 记忆化搜索+二分查找、动态规划

---
+++ training/src/leetcode/_403_canCross.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class _403_canCross {
+//    记忆化搜索+二分查找
+    private Boolean[][] rec;
+    public boolean canCross1(int[] stones) {
+        int n = stones.length;
+        rec = new Boolean[n][n];
+        return dfs(stones, 0, 0);
+    }
+    private boolean dfs(int[] stones, int i, int lastDis) {
+        if (i == stones.length - 1) {
+            return true;
+        }
+        if (rec[i][lastDis] != null) {
+            return rec[i][lastDis];
+        }
+
+        for (int curDis = lastDis - 1; curDis <= lastDis + 1; curDis++) {
+            if (curDis > 0) {
+                int j = Arrays.binarySearch(stones, i + 1, stones.length, curDis + stones[i]);
+                if (j >= 0 && dfs(stones, j, curDis)) {
+                    return rec[i][lastDis] = true;
+                }
+            }
+        }
+        return rec[i][lastDis] = false;
+    }
+
+//    动态规划
+    public boolean canCross2(int[] stones) {
+        int n = stones.length;
+        boolean[][] dp = new boolean[n][n];
+        dp[0][0] = true;
+        for (int i = 1; i < n; ++i) {
+            if (stones[i] - stones[i - 1] > i) {
+                return false;
+            }
+        }
+        for (int i = 1; i < n; ++i) {
+            for (int j = i - 1; j >= 0; --j) {
+                int k = stones[i] - stones[j];
+                if (k > j + 1) {
+                    break;
+                }
+                dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1];
+                if (i == n - 1 && dp[i][k]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
